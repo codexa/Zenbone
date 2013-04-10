c14d5035c1da9a857575bc09befc379482e54d35
Joshua Smith <joshua-smith@verizon.net>
2013-04-10 17:13:57 -0400

    Add results() function

--- chatterbox.js
+++ chatterbox.js
@@ -1,12 +1,16 @@
 // This script is by the Chatterbox contributors.
 
 var chatterbox = {};
+var results = new Array();
 
 chatterbox.send = function (content, address, type, sendMethod) {
+  results = new Array();
   if (address === '' | address == null | address == undefined) {
+    results.push('error: address not specified.');
     return 'error: address not specified.';
   }
   if (content === '' | content == null | content == undefined) {
+    results.push('error: nothing to send.');
     return 'error: nothing to send.';
   }
   if (type === '' | type == null | type == undefined) {
@@ -22,6 +26,8 @@ chatterbox.send = function (content, address, type, sendMethod) {
       chatterbox.makeCall(content, address);
     }
   }
+  results.push('Success: The send function completed.');
+  return 'Success: The send function completed.';
 }
 
 chatterbox.checkType = function (address) {
@@ -30,30 +36,46 @@ chatterbox.checkType = function (address) {
 
 chatterbox.sendEmail =  function (content, address) {
   if (address === '' | address == null | address == undefined) {
+    results.push('error: address not specified.');
     return 'error: address not specified.';
   }
   if (content === '' | content == null | content == undefined) {
+    results.push('error: nothing to send.');
     return 'error: nothing to send.';
   }
   // Some code to send email.
+  results.push('error: Please teach me how to send email.');
+  return 'error: Please teach me how to send email.';
 }
 
 chatterbox.sendText =  function (content, address) {
   if (address === '' | address == null | address == undefined) {
+    results.push('error: address not specified.');
     return 'error: address not specified.';
   }
   if (content === '' | content == null | content == undefined) {
+    results.push('error: nothing to send.');
     return 'error: nothing to send.';
   }
   // Some code to send a text.
+  results.push('error: Please teach me how to send texts.');
+  return 'error: Please teach me how to send texts.';
 }
 
 chatterbox.makeCall =  function (content, address) {
   if (address === '' | address == null | address == undefined) {
+    results.push('error: address not specified.');
     return 'error: address not specified.';
   }
   if (content === '' | content == null | content == undefined) {
+    results.push('error: nothing to send.');
     return 'error: nothing to send.';
   }
   // Some code to make a call.
+  results.push('error: Please teach me how to call people.');
+  return 'error: Please teach me how to call people.';
+}
+
+chatterbox.results = function() {
+  return results.toString();
 }
