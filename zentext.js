434a4013f0217e993c0dae8dd575c11ad38c54f2
Jack Doiron <jack83307@gmail.com>
2013-05-10 17:51:20 -0400

    removed zentext prefix from zentextResult the zentext prefix is no longer needed because of encapsulation

--- zentext.js
+++ zentext.js
@@ -4,7 +4,7 @@ var zentext = (function () {
   var results = new Array();
   var zentext = new Object();
   
-  function zentextResult(text, type) {
+  function result(text, type) {
     if (type != 'error' && type != 'success') {
       type = 'result';
     }
@@ -16,10 +16,10 @@ var zentext = (function () {
   zentext.send = function (content, address, type, sendMethod) {
     results = new Array();
     if (address == '' || address == null || address == undefined) {
-      return zentextResult('Address not specified.', 'error');
+      return result('Address not specified.', 'error');
     }
     if (content == '' || content == null || content == undefined) {
-      return zentextResult('Nothing to send.', 'error');
+      return result('Nothing to send.', 'error');
     }
     if (type == '' || type == null || type == undefined) {
       type = this.checkType(address);
@@ -34,47 +34,47 @@ var zentext = (function () {
         this.makeCall(content, address);
       }
     }
-    zentextResult('The send function completed.', 'success');
-    zentextResult('Sent ' + sendMethod + ' message.', 'success');
+    result('The send function completed.', 'success');
+    result('Sent ' + sendMethod + ' message.', 'success');
   };
   
   zentext.checkType = function (address) {
     // Some code to detect the type of message we want to send.  Will return 'mobile' if telephone number, 'email' if email.
-    return zentextResult('Please teach me how to check an address\'s type.', 'error');
+    return result('Please teach me how to check an address\'s type.', 'error');
   };
   
   zentext.sendEmail = function (content, address) {
     if (address == '' || address == null || address == undefined) {
-      return zentextResult('Address not specified.', 'error');
+      return result('Address not specified.', 'error');
     }
     if (content == '' || content == null || content == undefined) {
-      return zentextResult('Nothing to send.', 'error');
+      return result('Nothing to send.', 'error');
     }
     // Some code to send email.
-    return zentextResult('Please teach me how to send email.', 'error');
+    return result('Please teach me how to send email.', 'error');
   };
   
   zentext.sendText = function (content, address) {
     if (address == '' || address == null || address == undefined) {
-      return zentextResult('Address not specified.', 'error');
+      return result('Address not specified.', 'error');
     }
     if (content == '' || content == null || content == undefined) {
-      return zentextResult('Nothing to send.', 'error');
+      return result('Nothing to send.', 'error');
     }
     // Some code to send a text.
-    return zentextResult('Please teach me how to send texts.', 'error');
+    return result('Please teach me how to send texts.', 'error');
   };
   
   zentext.makeCall = function (content, address) {
     if (address == '' || address == null || address == undefined) {
-      return zentextResult('Address not specified.', 'error');
+      return result('Address not specified.', 'error');
     }
     if (content == '' || content == null || content == undefined) {
-      return zentextResult('Nothing to send.', 'error');
+      return result('Nothing to send.', 'error');
     }
     // Some code to make a call.
     results.push('error: Please teach me how to call people.');
-    return zentextResult('Please teach me how to call people.', 'error');
+    return result('Please teach me how to call people.', 'error');
   };
   
   zentext.results = function(format) {
