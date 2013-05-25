5eb706d787b867dcd0c4ac39d5b1d4e84f72f372
Jack Doiron <jack83307@gmail.com>
2013-05-25 11:19:21 -0400

    implemented email sending using mandrill and installed dependancies

---
+++ main.py
@@ -0,0 +1,28 @@
+import webapp2
+import mandrill
+
+def main():
+	global key
+	keyfile=open("key.txt","r");
+	key=keyfile.read();
+	keyfile.close();
+
+class EmailHandler(webapp2.RequestHandler):
+	def get(self):
+		message={}
+		to_address=[]
+		m=mandrill.Mandrill(key)
+		to_address.append({"email":str(self.request.get("to"))})
+		message["to"]=to_address
+		message["from_email"]=str(self.request.get("from"))
+		message["subject"]=str(self.request.get("subject"))
+		message["text"]=str(self.request.get("contents"))
+		self.response.write(m.messages.send(message))
+		self.response.write(message)
+
+app = webapp2.WSGIApplication([
+	webapp2.Route(r"/email", handler=EmailHandler, name="email")
+], debug=True)
+
+if __name__=="main":
+	main()
